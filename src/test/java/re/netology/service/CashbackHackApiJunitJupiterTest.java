5570793a5d6418c24236c05f6ddfa8618524d246
Andrey Shchepkin <lovenfed@gmail.com>
2021-08-29 14:07:26 +0300

    added JUnit Jupiter dependencies

---
+++ src/test/java/re/netology/service/CashbackHackApiJunitJupiterTest.java
@@ -0,0 +1,47 @@
+package re.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackApiJunitJupiterTest {
+    CashbackHackService cashbackHackService = new CashbackHackService();
+
+    @Test
+    public void shouldCalculateHowMuchBuy() {
+        int actual = cashbackHackService.remain(300);
+        int expected = 700;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateHowMuchBuy2() {
+        //В этом тесте ошибка
+        int actual = cashbackHackService.remain(1000);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+
+        @Test
+        public void shouldCalculateHowMuchBuy3 () {
+            int actual = cashbackHackService.remain(0);
+            int expected = 1000;
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void shouldCalculateHowMuchBuy4 () {
+            int actual = cashbackHackService.remain(1800);
+            int expected = 200;
+            assertEquals(expected, actual);
+
+        }
+        @Test
+        public void shouldCalculateHowMuchBuy5 () {
+            int actual = cashbackHackService.remain(744);
+            int expected = 256;
+            assertEquals(expected, actual);
+
+        }
+    }
--- src/test/java/re/netology/service/CashbackHackServiceTest.java
+++ src/test/java/re/netology/service/CashbackHackServiceTest.java
@@ -13,14 +13,14 @@ public class CashbackHackServiceTest {
         assertEquals(expected,actual);
 
     }
-//    @Test
-//    public void shouldCalculateHowMuchBuy2() {
-//        //В этом тесте ошибка
-//        int actual = cashbackHackService.remain(1000);
-//        int expected = 0;
-//        assertEquals(expected,actual);
-//
-//    }
+    @Test
+    public void shouldCalculateHowMuchBuy2() {
+        //В этом тесте ошибка
+        int actual = cashbackHackService.remain(1000);
+        int expected = 0;
+        assertEquals(expected,actual);
+
+    }
     @Test
     public void shouldCalculateHowMuchBuy3() {
         int actual = cashbackHackService.remain(0);
